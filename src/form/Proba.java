6cb9d18dcd33ee57e50cae84683db2b08b225bae
avfyodorov <avfyodorov60@yandex.ru>
2021-01-18 18:35:09 +0300

    conn to mysql

---
+++ src/form/Proba.java
@@ -0,0 +1,22 @@
+package form;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Proba {
+   public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//      String driver = "com.mysql.jdbc.Driver";
+//      String connection = "jdbc:mysql://localhost:3306/world";
+//      String user = "root";
+//      String password = "12345";
+//      Class.forName(driver);
+//      Connection con = DriverManager.getConnection(connection, user, password);
+//      if (!con.isClosed()) {
+//         con.close();
+//         System.out.println("Con closed");
+//      } else
+//         System.out.println("Ypa");
+   }
+}
